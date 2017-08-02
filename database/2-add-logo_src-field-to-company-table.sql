127d4036da46ca4b021807d9cdaca327ecfb2fb3
PomianAurelian <fruits_go_wild@yahoo.com>
2017-08-02 12:58:13 +0300

    Add logo_src field to company table.

---
+++ database/2-add-logo_src-field-to-company-table.sql
@@ -0,0 +1,7 @@
+ALTER TABLE company ADD logo_src varchar(255);
+UPDATE company SET logo_src = 'square.png' WHERE id = 6;
+UPDATE company SET logo_src = 'square2.jpg' WHERE id = 7;
+UPDATE company SET logo_src = 'square3.png' WHERE id = 8;
+UPDATE company SET logo_src = 'square4.gif' WHERE id = 9;
+UPDATE company SET logo_src = 'square5.jpg' WHERE id = 10;
+UPDATE company SET logo_src = 'square6.png' WHERE id = 11;
